5ff559037a0a2c8a6292c8dfa9a983879bcc963b
Jason Somai <jsomai@Jasons-MacBook-Pro.local>
2019-10-16 23:25:39 -0400

    more commits

--- functions.php
+++ functions.php
@@ -23,15 +23,28 @@ if ( ! defined( 'ABSPATH' ) ) {
 function wpgt_scripts() {
 	// Frontend scripts.
 	if ( ! is_admin() ) {
-		// Enqueue vendors first.
-		wp_enqueue_script( 'wpgt_vendorsJs', get_template_directory_uri() . '/assets/js/vendors.min.js' );
 
-		// Enqueue custom JS after vendors.
+		wp_enqueue_script( 'sticky-sidebar', get_template_directory_uri() . '/assets/js/jquery.sticky-sidebar.min.js', array(), '1', true );
+
+		wp_enqueue_script( 'lightcase', get_template_directory_uri() . '/assets/js/lightcase.js', array(), '1', true );
+
+		wp_enqueue_script( 'wpgt_vendorsJs', get_template_directory_uri() . '/assets/js/vendor.min.js' );
+
 		wp_enqueue_script( 'wpgt_customJs', get_template_directory_uri() . '/assets/js/custom.min.js' );
 
-		// Minified and Concatenated styles.
 		wp_enqueue_style( 'wpgt_style', get_template_directory_uri() . '/style.min.css', array(), '1.0', 'all' );
 	}
 }
 // Hook.
 add_action( 'wp_enqueue_scripts', 'wpgt_scripts' );
+
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page();
+	
+}
+
+function yoasttobottom() {
+	return 'low';
+  }
+  add_filter( 'wpseo_metabox_prio', 'yoasttobottom');
