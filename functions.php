cf050837f5657c1a0b57e13ee753056d1126300b
Jason Somai <jsomai458@gmail.com>
2020-11-14 12:41:54 -0500

    added js libraries

--- functions.php
+++ functions.php
@@ -24,15 +24,31 @@ function wpgt_scripts() {
 	// Frontend scripts.
 	if ( ! is_admin() ) {
 
-		wp_enqueue_script( 'sticky-sidebar', get_template_directory_uri() . '/assets/js/jquery.sticky-sidebar.min.js', array(), '1', true );
+		// Styles
+		wp_enqueue_style( 'aos_style', get_template_directory_uri() . '/assets/vendor/aos/aos.css',  array(), '1.0', 'all'  );
 
-		wp_enqueue_script( 'lightcase', get_template_directory_uri() . '/assets/js/lightcase.js', array(), '1', true );
+		wp_enqueue_style( 'lightcase_style', get_template_directory_uri() . '/assets/vendor/lightcase/css/lightcase.css',  array(), '1.0', 'all'  );
 
-		wp_enqueue_script( 'wpgt_vendorsJs', get_template_directory_uri() . '/assets/js/vendor.min.js' );
-
-		wp_enqueue_script( 'wpgt_customJs', get_template_directory_uri() . '/assets/js/custom.min.js' );
+		wp_enqueue_style( 'wpgt_style_slick', get_template_directory_uri() . '/assets/vendor/slick/slick.css',  array(), '1.0', 'all'  );
 
 		wp_enqueue_style( 'wpgt_style', get_template_directory_uri() . '/style.min.css', array(), '1.1', 'all' );
+
+		// Scripts
+		wp_enqueue_script( 'aos_js', get_template_directory_uri() . '/assets/vendor/aos/aos.js' );
+
+		wp_enqueue_script( 'lightcase_js', get_template_directory_uri() . '/assets/vendor/lightcase/js/lightcase.js', array('jquery'), '1.0', false  );
+
+		wp_enqueue_script( 'wpgt_slickJs', get_template_directory_uri() . '/assets/vendor/slick/slick.min.js', array('jquery'), '1.0', true );
+
+		wp_enqueue_script( 'sticky-sidebar', get_template_directory_uri() . '/assets/js/jquery.sticky-sidebar.min.js', array(), '1', true );
+
+		// Enqueue vendor JS after vendors to call those libraries.
+		wp_enqueue_script( 'wpgt_vendorsJs', get_template_directory_uri() . '/assets/js/vendor.min.js', array('jquery'), '1.0', true );
+
+		// Enqueue custom JS last.
+		wp_enqueue_script( 'wpgt_customJs', get_template_directory_uri() . '/assets/js/custom.min.js', array('jquery'), '1.0', true );
+
+		
 	}
 }
 // Hook.
@@ -54,7 +70,7 @@ function register_menus() {
    return 25;
    }
    add_filter( 'excerpt_length', 'tn_custom_excerpt_length', 999 );
-
+   remove_filter( 'the_content', 'wpautop' );
    remove_filter ('acf_the_content', 'wpautop');
 add_action( 'wp_enqueue_scripts', 'wpgt_scripts' );
 
