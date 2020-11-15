398d9a325d12b11c8d45415c96fb0aefb5e4b8de
Jason Somai <jsomai458@gmail.com>
2020-11-15 18:54:12 -0500

    added tariqa lightbox and html structure

--- tariqa.php
+++ tariqa.php
@@ -10,28 +10,41 @@ get_template_part( 'template-parts/page-header');
 
 <?php if ( have_rows( 'intro_section' ) ) : ?>
 <?php while ( have_rows( 'intro_section' ) ) : the_row(); ?>
-<section id="tariqa-intro">
+<section id="tariqa-intro" class="intro">
 	<div class="container">
-		<div class="row">
-			<div class="col">
+		<div class="row justify-content-center">
+			<div class="col-12 col-lg-8 text-center">
 				<h2><?php the_sub_field( 'header' ); ?></h2>
 				<div class="regular-text"><?php the_sub_field( 'content' ); ?></a>
-					<a href="<?php the_sub_field( 'link' ); ?>"><?php the_sub_field( 'link_text' ); ?></a>
+					<a href="<?php the_sub_field( 'link' ); ?>" class="btn-ar"><?php the_sub_field( 'link_text' ); ?></a>
 				</div>
 			</div>
 		</div>
 </section>
 <?php endwhile; ?>
 <?php endif; ?>
-<?php
-if( have_rows('timeline') ):
-while ( have_rows('timeline') ) : the_row();?>
-<div class="timebox">
-<h3><?php the_sub_field( 'year' ); ?></h3>
-<h4><?php the_sub_field( 'name' ); ?></h4>
-<div class="content">
-<?php the_sub_field( 'content' ); ?>
-</div>
-</div>
-<?php endwhile; endif; ?>
+
+<section id="tariqa">
+	<div class="container">
+		<?php
+if( have_rows('timeline') ): $i = 0;
+while ( have_rows('timeline') ) : the_row(); $i++;?>
+		<div class="row <?php if(!($i % 2 == 0)): echo 'justify-content-end'; endif;?>">
+			<div class="timebox col-12 col-lg-6">
+				<a href="<?php echo '#popup-'.$i;?>" data-rel="lightcase">
+				<h3><?php the_sub_field( 'year' ); ?></h3>
+				<h4 class="sub"><?php the_sub_field( 'name' ); ?></h4>
+				
+				</a>
+				<div id="<?php echo 'popup-'.$i;?>" style="display:none;">
+				<div class="content">
+					<?php the_sub_field( 'content' ); ?>
+				</div>
+				</div>
+			</div>
+		</div>
+			<?php endwhile; endif; ?>
+	</div>
+</section>
+
 <?php get_footer(); ?>
